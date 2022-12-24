25f8659b183fde443abb6b4a3e43ab278fb2bc94
Samriddh Negi <samridh.negi007@gmail.com>
2022-12-24 10:46:31 +0530

    added sliding background image animation

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var slideImg=document.getElementById("slideImg");
-var images =new Array(
-    "/Images/1.jpg",
-    "/Images/2.jpg",
-    "/Images/3.jpg",
-    "/Images/4.jpg"
-);
-function Queen() {
-    slideImg.src="/Images/3.jpg"
-}
-
-function Gorillaz()
-{
-    slideImg.src=images[2];
-}
+var slideImg = document.getElementById("")
